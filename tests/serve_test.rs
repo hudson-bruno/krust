5992ed91769efb8681972a26619edcb5e122b976
Hudson Bruno <34108140+hudson-bruno@users.noreply.github.com>
2025-09-14 01:58:43 -0300

    test: add response same request correlation id test

--- tests/serve_test.rs
+++ tests/serve_test.rs
@@ -1,19 +1,26 @@
-use codecrafters_kafka::response::{KafkaResponse, KafkaResponseHeader};
+use codecrafters_kafka::request::{KafkaRequest, KafkaRequestHeader};
 
 mod common;
 use common::TestContext;
 
 #[tokio::test]
-async fn test_parse_correlation_id() {
+async fn test_response_same_request_correlation_id() {
     let mut ctx = TestContext::new().await;
 
-    let request = KafkaResponse {
+    let request = KafkaRequest {
         message_size: 0,
-        header: KafkaResponseHeader { correlation_id: 7 },
+        header: KafkaRequestHeader {
+            request_api_key: 0,
+            request_api_version: 0,
+            correlation_id: 7,
+        },
     };
     ctx.send_request(&request).await.unwrap();
 
-    let response = ctx.parse_request().await.unwrap();
+    let response = ctx.parse_response().await.unwrap();
 
-    assert_eq!(request, response);
+    assert_eq!(
+        request.header.correlation_id,
+        response.header.correlation_id
+    );
 }
