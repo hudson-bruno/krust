2482440aac57a941f2a6153754287340eeea53c8
Hudson Bruno <34108140+hudson-bruno@users.noreply.github.com>
2025-09-14 01:03:14 -0300

    test: add basic test setup

---
+++ tests/common.rs
@@ -0,0 +1,44 @@
+use std::io;
+
+use codecrafters_kafka::request::KafkaRequest;
+use tokio::{
+    net::{TcpListener, TcpStream},
+    task::JoinHandle,
+};
+
+pub struct TestContext {
+    pub serve_handle: JoinHandle<()>,
+    pub client_io: TcpStream,
+}
+
+impl TestContext {
+    pub async fn new() -> Self {
+        let listener = TcpListener::bind(("0.0.0.0", 0)).await.unwrap();
+        let listener_addr = listener.local_addr().unwrap();
+
+        let serve_handle = tokio::spawn(async {
+            codecrafters_kafka::serve(listener).run().await.unwrap();
+        });
+
+        let client_io = TcpStream::connect(listener_addr).await.unwrap();
+
+        Self {
+            serve_handle,
+            client_io,
+        }
+    }
+
+    pub async fn parse_request(&mut self) -> io::Result<KafkaRequest> {
+        KafkaRequest::from_reader(&mut self.client_io).await
+    }
+
+    pub async fn send_request(&mut self, request: &KafkaRequest) -> io::Result<()> {
+        request.write_into(&mut self.client_io).await
+    }
+}
+
+impl Drop for TestContext {
+    fn drop(&mut self) {
+        self.serve_handle.abort();
+    }
+}
