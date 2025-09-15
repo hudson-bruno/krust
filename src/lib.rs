f78a190c74ddc49ccf4766ab6d97f714bc71e201
Hudson Bruno <34108140+hudson-bruno@users.noreply.github.com>
2025-09-14 23:06:13 -0300

    feat: now a single connection can process multiple requests in series

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use std::{io, net::SocketAddr};
 
 use tokio::{
-    io::AsyncWriteExt,
     net::{TcpListener, TcpStream},
     time::Instant,
 };
@@ -38,58 +37,64 @@ async fn handle_connection(mut io: TcpStream, remote_addr: SocketAddr) {
     tokio::spawn(async move {
         tracing::trace!("connection {remote_addr:?} accepted");
 
-        let start_time = Instant::now();
+        loop {
+            let start_time = Instant::now();
 
-        let request = KafkaRequest::from_reader(&mut io).await.unwrap();
-        tracing::debug!("request: {:?}", request);
+            let response = handle_package(&mut io).await;
 
-        let response = if request.header.api_key == 18
-            && request.header.api_version >= 0
-            && request.header.api_version <= 4
-        {
-            KafkaResponse {
-                header: KafkaResponseHeader {
-                    correlation_id: request.header.correlation_id,
-                },
-                body: KafkaResponseBody {
-                    api_versions: vec![
-                        ApiVersion {
-                            api_key: 1,
-                            max_supported_api_version: 17,
-                            ..ApiVersion::default()
-                        },
-                        ApiVersion {
-                            api_key: 18,
-                            max_supported_api_version: 4,
-                            ..ApiVersion::default()
-                        },
-                        ApiVersion {
-                            api_key: 75,
-                            ..ApiVersion::default()
-                        },
-                    ],
-                    ..KafkaResponseBody::default()
-                },
-            }
-        } else {
-            KafkaResponse {
-                header: KafkaResponseHeader {
-                    correlation_id: request.header.correlation_id,
-                },
-                body: KafkaResponseBody {
-                    error_code: 35,
-                    ..KafkaResponseBody::default()
-                },
-            }
-        };
-
-        response.write_into(&mut io).await.unwrap();
-
-        io.shutdown().await.unwrap();
-
-        let elapsed_time = start_time.elapsed();
-        tracing::debug!("response: {:?} elapsed: {:?}", response, elapsed_time);
+            let elapsed_time = start_time.elapsed();
+            tracing::debug!("response: {:?} elapsed: {:?}", response, elapsed_time);
+        }
     });
+}
+
+async fn handle_package(mut io: &mut TcpStream) -> KafkaResponse {
+    let request = KafkaRequest::from_reader(&mut io).await.unwrap();
+    tracing::debug!("request: {:?}", request);
+
+    let response = if request.header.api_key == 18
+        && request.header.api_version >= 0
+        && request.header.api_version <= 4
+    {
+        KafkaResponse {
+            header: KafkaResponseHeader {
+                correlation_id: request.header.correlation_id,
+            },
+            body: KafkaResponseBody {
+                api_versions: vec![
+                    ApiVersion {
+                        api_key: 1,
+                        max_supported_api_version: 17,
+                        ..ApiVersion::default()
+                    },
+                    ApiVersion {
+                        api_key: 18,
+                        max_supported_api_version: 4,
+                        ..ApiVersion::default()
+                    },
+                    ApiVersion {
+                        api_key: 75,
+                        ..ApiVersion::default()
+                    },
+                ],
+                ..KafkaResponseBody::default()
+            },
+        }
+    } else {
+        KafkaResponse {
+            header: KafkaResponseHeader {
+                correlation_id: request.header.correlation_id,
+            },
+            body: KafkaResponseBody {
+                error_code: 35,
+                ..KafkaResponseBody::default()
+            },
+        }
+    };
+
+    response.write_into(&mut io).await.unwrap();
+
+    response
 }
 
 pub trait Serializable {
